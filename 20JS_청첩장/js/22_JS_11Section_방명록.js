f74d32382545554a8f1f5f3bef3ae1efdf05a67b
hongjaeseong <hjs971102@naver.com>
2024-04-12 13:43:09 +0900

    .

---
+++ 20JS_청첩장/js/22_JS_11Section_방명록.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const board_btn =  document.querySelector('.board_btn');
+board_btn.addEventListener('click',()=>{
+
+    console.log("!!!!!");
+
+    const innerFrame =  document.querySelector('.utterances-frame');
+    var iframeDocument = innerFrame.contentWindow.document;
+
+
+    console.log("iframeDocument",iframeDocument);
+
+})
+
+
+
